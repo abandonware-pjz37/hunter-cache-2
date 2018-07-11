061aaca35abe66d05b07e39ece6eea0ce8a821e1
ingenue <ingenue@users.noreply.github.com>
2018-07-11 21:21:00 +0000

    Uploading cache info

---
+++ 8fed42d/gst_plugins_ugly/1.10.4/a01ab3a/296fa44/args.cmake
@@ -0,0 +1,3 @@
+set("DEPENDS_ON_PACKAGES" "gst_plugins_base" CACHE INTERNAL "")
+set("DEPENDS_ON_PKGCONFIGS" "gstreamer-plugins-base-1.0" CACHE INTERNAL "")
+set("BUILD_SHARED_LIBS" "ON" CACHE INTERNAL "")
