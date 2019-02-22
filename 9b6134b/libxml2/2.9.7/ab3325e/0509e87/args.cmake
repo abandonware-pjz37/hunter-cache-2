05db855578169ff819da9186d2792277aec8c04a
ingenue <ingenue@users.noreply.github.com>
2019-02-22 13:22:49 +0000

    Uploading cache info
    Travis:
      TRAVIS_BRANCH: upload.libxml2
      TRAVIS_BUILD_ID: 497029196
      TRAVIS_BUILD_NUMBER: 5043
      TRAVIS_JOB_ID: 497029197
      TRAVIS_JOB_NUMBER: 5043.1
      TRAVIS_OS_NAME: linux
      TRAVIS_REPO_SLUG: ingenue/hunter
    
      Job URL: https://travis-ci.org/ingenue/hunter/jobs/497029197

---
+++ 9b6134b/libxml2/2.9.7/ab3325e/0509e87/args.cmake
@@ -0,0 +1,3 @@
+set("EXTRA_FLAGS" "--without-python" CACHE INTERNAL "")
+set("EXTRA_FLAGS" "${EXTRA_FLAGS}" "--without-lzma" CACHE INTERNAL "")
+set("EXTRA_FLAGS" "${EXTRA_FLAGS}" "--without-zlib" CACHE INTERNAL "")
